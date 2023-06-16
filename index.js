b5a94dd800de6ddd4f07727709dfcf14292fb5f0
Khubaib Ahmed <Khubaibahmed567@gmail.com>
2023-06-16 22:53:27 +0500

    final_deplo

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const path = require('path');
 const dotenv = require("dotenv") ;
+const mongoose = require("mongoose");
 
 const app = express();
 dotenv.config();
 
 const port = process.env.PORT || 5000;
+
+
+// Connected with MongoDB
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to mongoDB.");
+  } catch (error) {
+    throw error;
+  }
+};
+
+mongoose.connection.on("disconnected", () => {
+  console.log("mongoDB disconnected!");
+});
 
 // Serve static files from the 'build' directory
 app.use(express.static(path.join(__dirname, 'build')));
