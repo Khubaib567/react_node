a901e15d6a1a50358ab66d3519d75d6032ef1651
Khubaib Ahmed <Khubaibahmed567@gmail.com>
2023-06-16 23:01:23 +0500

    final_deploy(2)

--- index.js
+++ index.js
@@ -9,6 +9,20 @@ dotenv.config();
 const port = process.env.PORT || 5000;
 
 
+// Serve static files from the 'build' directory
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Serve the index.html file for all requests
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
+
+
 // Connected with MongoDB
 const connect = async () => {
   try {
@@ -23,15 +37,6 @@ mongoose.connection.on("disconnected", () => {
   console.log("mongoDB disconnected!");
 });
 
-// Serve static files from the 'build' directory
-app.use(express.static(path.join(__dirname, 'build')));
 
-// Serve the index.html file for all requests
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
-});
+connect()
